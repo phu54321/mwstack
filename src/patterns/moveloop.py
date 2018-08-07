92556160001da2a332c4722d59b90c7638845531
phu54321 <phu54321@naver.com>
2018-08-07 23:44:36 +0900

    Basic location settings

---
+++ src/patterns/moveloop.py
@@ -0,0 +1,15 @@
+import patterns.locSetting as locSetting
+from eudplib import *
+
+@EUDFunc
+def f_moveLoop():
+    for cPlayer in [P7, P8]:
+        locSetting.f_setMissileLocations(cPlayer)
+        for unit in ['Fast Missile', 'Missile', 'Slow Missile']:
+            DoActions([
+                [
+                    Order(unit, cPlayer, locSetting.vFromArray[x], Move, locSetting.vToArray[x]),
+                    KillUnitAt(All, unit, locSetting.vToArray[x], cPlayer)
+                ]
+                for x in range(11)
+            ])
